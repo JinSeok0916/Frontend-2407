b8e9b53f403db16da285f78cc03dd7daddd2aae6
JinSeok Choi <gkgjfj@naver.com>
2024-07-15 10:59:02 +0900

    JS

---
+++ JS/ch02.자료와변수/01.자료형.js
@@ -0,0 +1,11 @@
+/*
+ * 자바스크립트의 자료형 
+*/
+
+// 1. 문자열(String)
+console.log("He said 'i love you.'");
+console.log('He said "i love you."');
+console.log("I'll be back.");
+
+// 특수문자 - 백슬래시(\) + 문자   \t tab 한 번, \n 줄바꿈
+console.log("\tabc\tdef\"ghi\njkl\\");
