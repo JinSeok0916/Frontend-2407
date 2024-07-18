18141c356843b2a42d5a003b302d780204339255
JinSeok Choi <gkgjfj@naver.com>
2024-07-18 13:14:04 +0900

    Javascript

---
+++ JS/ch05.함수/07.minMax.js
@@ -0,0 +1,41 @@
+// 최소값, 최대값 찾기
+
+let arr = [45, 3, 27, 89, 6, 24];
+let minVal = 0;
+let maxVal = 0;
+
+for (i = 1; arr.length >= i; i++) {
+    minVal += arr[arr.length - i];
+}
+for (i = 1; arr.length >= i; i++) {
+    maxVal -= arr[arr.length - i];
+}
+
+console.log(`${minVal}, ${maxVal}`);
+
+for (let num of arr) {
+    if (num < minVal) {
+        minVal = num
+    }
+    if (num > maxVal) {
+        maxVal = num
+    }
+    console.log(`과정: ${num}, 최소값: ${minVal}, 최대값: ${maxVal}`);
+}
+console.log(`최소값: ${minVal}, 최대값: ${maxVal}`);
+
+// function myMin(arr) {
+//     let minVal = Number.MAX_SAFE_INTEGER;
+//     for (let num of arr)
+//         if (num < minVal)
+//             minVal = num;
+//     return minVal;
+// }
+// function myMax(arr) {
+//     let maxVal = Number.MIN_SAFE_INTEGER;
+//     for (let num of arr)
+//         if (num > maxVal)
+//             maxVall = num;
+//     return maxVall;
+// }
+// console.log(myMin(arr), myMax(arr));
---
+++ JS/ch05.함수/08.분산.js
@@ -0,0 +1,11 @@
+// 배열 원소 각각의 제곱의 합
+
+function sumOfSquare(arr) {
+
+}
+
+// 배열 원소의 합의 제곱
+
+function squareOfSum(arr) {
+
+}
